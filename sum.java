57cbe159bed13a5d061a3887aaf1401818251361
Mohit Shishodia <mohitsisodia667@gmail.com>
2022-05-15 13:12:26 +0530

    added message in sum.java

---
+++ sum.java
@@ -0,0 +1,8 @@
+public class LEET {
+    public static void main(String[] args) {
+        int[]num={4,3,5};
+        int sum=num[0]+num[1];
+        System.out.println("The"+sum+"two number");
+        
+    }
+}
